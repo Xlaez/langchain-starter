8ed019c78bb093d109639c7042946fd58ef03751
xlaez <utibeabasiekong2@gmail.com>
2023-08-05 23:43:09 +0100

    first official llm implementation with embeddings and openai

---
+++ main.py
@@ -0,0 +1,33 @@
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain import OpenAI
+from langchain.document_loaders import DirectoryLoader
+from langchain.chains  import RetrievalQA
+from langchain.chains.question_answering import load_qa_chain
+import magic
+import os
+import nltk
+
+# Remove API key before commiting
+
+loader = DirectoryLoader('./data', glob='**/*.txt*')
+document = loader.load()
+
+text_splitter = CharacterTextSplitter(separator="\n\n")
+texts = text_splitter.split_documents(document)
+
+embeddings = OpenAIEmbeddings(openai_api_key=os.environ['OPENAI_API_KEY'])
+
+docSearch = Chroma.from_documents(texts, embeddings)
+# Initialize model
+
+qa_chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
+
+qa = RetrievalQA(combine_documents_chain=qa_chain, retriever=docSearch.as_retriever(search_kwargs={"k":1}))
+
+query =  "What are the main problems in the Nigerian education system?"
+answer = qa.run(query)
+print(answer)
+
+
